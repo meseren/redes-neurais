2684e81b3054d69526cd3feb56a284c06c697b94
Melissa Seren <mee_seren@outlook.com>
2020-06-10 21:56:29 -0300

    finalização pela graça de deus pai amém

--- RedeNeural.php
+++ RedeNeural.php
@@ -1,141 +1,127 @@
 <?php 
 class RedeNeural{  
 
-    private $alfa = 0.1;
-    private $pesos_h;
-    private $pesos_o;
-    private $erros;
-    private $erros_2;
-    private $H1;
-    private $H2;
-    private $taxaAprendizado = 0.2;
-    private $delta_h1;
-    private $delta_h2;
-    private $delta_h1_anterior;
-    private $delta_h2_anterior;
-    private $delta_o1;
-    private $delta_o1_anterior;
-    private $delta_o2;
-    private $delta_o2_anterior;
-    private $gradienteEntradas;
-    private $gradienteCamadaOculta;
-    private $entradas;
-    private $erroQuadratico;
-    private $erroQuadratico_2;
-    private $resultadoEsperado;
-    private $resultadoEsperado_2;
-    private $resultados;
-    private $resultados_2;
+    private $pesos;
     private $e = 2.7183;
 
-    public function __construct($entradas, $resultadoEsperado, $resultadoEsperado_2, $pesos_h, $pesos_o) {
-        $this->entradas = $entradas;
-        $this->resultadoEsperado = $resultadoEsperado;
-        $this->resultadoEsperado_2 = $resultadoEsperado_2;
-        $this->pesos_h = $pesos_h;
-        $this->pesos_o = $pesos_o;
-        $this->taxaAprendizado = 0.2;
+    public function __construct($pesos) {
+        $this->pesos = $pesos;
     }
-    
-    public function getMse(){
-        //Para todas as entradas, é calculado um erro
-        foreach($this->entradas as $key => $x){
-            $neuronio = new Neuronio($x, $this->pesos_h[0]);
-            $this->H1[] =  $neuronio->getSaida();
-    
-            $neuronio = new Neuronio($x, $this->pesos_h[1]);
-            $this->H2[] =  $neuronio->getSaida();
-        
-            $u_1 = $this->pesos_o[0][0] + $this->H1[$key] * $this->pesos_o[0][1] + $this->H2[$key] * $this->pesos_o[0][2];
-            $u_2 = $this->pesos_o[0][0] + $this->H1[$key] * $this->pesos_o[1][1] + $this->H2[$key] * $this->pesos_o[1][2];        
 
-            $this->resultados[] = 1/(1 +  pow($this->e, -$u_1));
-            $this->erros[] = $this->resultadoEsperado[$key] - $this->resultados[$key];
-            $this->erroQuadratico += pow($this->erros[$key], 2);
+    public function Y($neuronio, $entrada){
+        $u = $this->pesos[$neuronio][0];
 
-            $this->resultados_2[] = 1/(1 +  pow($this->e, -$u_2));
-            $this->erros_2[] = $this->resultadoEsperado_2[$key] - $this->resultados_2[$key];
-            $this->erroQuadratico += pow($this->erros_2[$key], 2);
+        for ($i=0; $i < count($entrada); $i++) { 
+            $u += $entrada[$i] * $this->pesos[$neuronio][$i+1];
         }
-        
-        return ($this->erroQuadratico/(count($this->entradas)*2))*100;
+
+        return 1/(1 +  pow($this->e, -$u));
     }
 
-    public function atualizaPesos(){
-        $i=0;
+    public function MLP($entrada){
+        $H1 = $this->Y(0, $entrada);
+        $H2 = $this->Y(1, $entrada);
+        $O1 = $this->Y(2, [$H1, $H2]);
+        $O2 = $this->Y(3, [$H1, $H2]);
 
+        return [$O1,$O2,$H1,$H2];
+    }
+
+    public function MSE($padroes, $resultados_esperados){
+        $s = 0;
+
+        for ($i=0; $i<count($padroes); $i++) { 
+            $Ys = $this->MLP($padroes[$i]);
+            $EO1 = pow(($resultados_esperados[$i][0] - $Ys[0]), 2);
+            $EO2 = pow(($resultados_esperados[$i][1] - $Ys[1]), 2);
+            $s += $EO1 + $EO2;
+        }
+
+        return ($s/(count($padroes)*2))*100;
+    }
+
+    public function treinar($padroes, $resultados_esperados, $eta, $alpha){
+        
         $dp[0] = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]];
         $dp[1] = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]];
         $dp[2] = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]];
         $dp[3] = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]];
 
-        while(($this->getMse() > 0.01) && ($i < 10000)){
-            foreach($this->entradas as $key => $value){
-                $this->calculaDelta();
-                $this->calculaGradiente();
-            }
-            // cada padrão calcula saida
-            // delta da camada de saida 
-            // oculta 
-            // gradiente 
-            // atualiza
-            // 4x
-            // mse dnv
-            $i++;
-        }
-    }
-    
+        $cont = 0;
 
-    public function calculaDelta(){
-     
-        foreach($this->entradas as $key => $x){
-            $derivadaFdeU = ($this->resultados[$key] * (1- $this->resultados[$key]));
-            $this->delta_o1[] =  $derivadaFdeU * $this->erros[$key];
+        while(($this->MSE($padroes, $resultados_esperados)>0.01) && ($cont < 100000)){
 
-            $derivadaFdeU = ($this->resultados_2[$key] * (1- $this->resultados_2[$key]));
-            $this->delta_o2[] =  $derivadaFdeU * $this->erros_2[$key];
-            
-            $derivadaFdeU = ($this->H1[$key] * (1 - $this->H1[$key]));
-            $this->delta_h1[] = $derivadaFdeU * (($this->pesos_o[0][1] * $this->delta_o1[$key]) + ($this->pesos_o[1][1] * $this->delta_o2[$key]));
-        
-            $derivadaFdeU = ($this->H2[$key] * (1 - $this->H2[$key]));
-            $this->delta_h2[] = $derivadaFdeU * (($this->pesos_o[0][2] * $this->delta_o1[$key]) + ($this->pesos_o[1][2] * $this->delta_o2[$key]));
-           
-        }
-    }
+            for ($i=0; $i<count($padroes); $i++) { 
+                $Ys = $this->MLP($padroes[$i]);
 
-    public function calculaGradiente(){
+                $D = $this->deltas($Ys, $resultados_esperados[$i]);
 
-        foreach($this->entradas as $i => $value){
-            foreach($value as $j => $x){
-                $this->gradienteEntradas['H1'][$i][$j] = $x * $this->delta_h1[$i];
-                $this->gradienteEntradas['H2'][$i][$j] = $x * $this->delta_h2[$i];
+                $g = $this->gradientes($Ys, $padroes[$i], $D);
+
+                $do[$i] = $dp[$i];
+
+                $dp[$i] = $this->deslocamento($g, $do[$i], $eta, $alpha);
+
+                $this->atualiza_pesos($dp[$i]);
             }
 
-            $this->gradienteCamadaOculta['01'][$i][] = 1 * $this->delta_o1[$i];    
-            $this->gradienteCamadaOculta['01'][$i][] = $this->H1[$i] * $this->delta_o1[$i];    
-            $this->gradienteCamadaOculta['01'][$i][] = $this->H2[$i] * $this->delta_o1[$i];    
-           
-            $this->gradienteCamadaOculta['02'][$i][] = 1 * $this->delta_o2[$i];    
-            $this->gradienteCamadaOculta['02'][$i][] = $this->H1[$i] * $this->delta_o2[$i];    
-            $this->gradienteCamadaOculta['02'][$i][] = $this->H2[$i] * $this->delta_o2[$i];  
+            $cont++;
         }
     }
 
-    public function delta(){
+    public function deltas($Ys, $resultados_desejados){
+        $E1 = $resultados_desejados[0] - $Ys[0];
+        $deltaO1 = $E1 * ($Ys[0] * (1 - $Ys[0]));
+
+        $E2 = $resultados_desejados[1] - $Ys[1];
+        $deltaO2 = $E2 * ($Ys[1] * (1 - $Ys[1]));
+
+        $deltaH1 =  ($Ys[2] * (1 - $Ys[2] )) * (($this->pesos[2][1] * $deltaO1) + ($this->pesos[3][1] * $deltaO2));
+        $deltaH2 =  ($Ys[3] * (1 - $Ys[3] )) * (($this->pesos[2][2] * $deltaO1) + ($this->pesos[3][2] * $deltaO2));
+
+        return [$deltaO1, $deltaO2, $deltaH1, $deltaH2];
+    }
+
+    public function gradientes($Ys, $entrada, $D) {
+
+        $gh10 = $D[2];
+        $gh11 = $entrada[0]*$D[2];
+        $gh12 = $entrada[1]*$D[2];
         
+        $gh20 = $D[3];
+        $gh21 = $entrada[0]*$D[3];
+        $gh22 = $entrada[1]*$D[3];
+
+        $go10 = $D[0];
+        $go11 = $Ys[2]*$D[0];
+        $go12 = $Ys[3]*$D[0];
+
+        $go20 = $D[1];
+        $go21 = $Ys[2]*$D[1];
+        $go22 = $Ys[3]*$D[1];
+
+        return [[$gh10, $gh11, $gh12],
+                [$gh20, $gh21, $gh22],
+                [$go10, $go11, $go12],
+                [$go20, $go21, $go22]];
     }
 
-    public function getDeslocamento(){
+    public function deslocamento($g, $do, $eta, $alpha){
+        for ($i=0; $i<count($g); $i++) { 
+            for ($j=0; $j<count($g[$i]); $j++) { 
+                $deslocamento[$i][$j] = $eta*$g[$i][$j]+$alpha*$do[$i][$j];
+            }
+        }
 
+        return $deslocamento;
     }
 
-    public function getResultados(){
-        return $this->resultados;
-    }
-
-    public function getErroQuadratico(){
-        return $this->erroQuadratico;
+    public function atualiza_pesos($dp){
+        for ($i=0; $i<count($dp); $i++) {
+            for ($j=0; $j <count($dp[$i]) ; $j++) { 
+                $this->pesos[$i][$j] += $dp[$i][$j];
+            }
+        }
     }
 }
 ?>
--- index_pt2.php
+++ index_pt2.php
@@ -1,39 +1,110 @@
 <?php 
 
-    include 'Neuronio.php';
+    /*
+        NOTAS DOS INTEGRANTES: Professor, como pôde acompanhar durante as aulas, estávamos conseguindo gerar os deltas e gradientes certinho, porém depois que vi o pseudo código do Sr, percebi que estava um tanto quanto errado e tive que alterar para a sua lógica rsrsrs. 
+
+        Nosso histórico de desenvolvimento está nos commits do GitHub, caso queira verificar: https://github.com/meseren/redes-neurais
+
+
+        Integrantes: Melissa, Allan e Leonardo Clemente 
+    */
     include 'RedeNeural.php';
 
     /**Inputs */
-
-    /* Exemplo 1 */
-    $entradas = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]];
-    $resultadoEsperado = [0,1,1,0];
-    $resultadoEsperado_2 = [1,0,0,1];
-    $pesos_h = [[-0.46,-0.7,0.22],[0.10,0.94,0.46]];
-    $pesos_o = [[0.78,-0.22,0.58], [0.78,-0.22,0.58]];
     
-    /* Exemplo 2
-    $entradas = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]];
-    $resultadoEsperado = [0,1,1,0];
-    $pesos_h = [[-0.16,-0.87,0.15],[0.90,0.14,0.05]];
-    $pesos_o = [0.20,-0.25,0.15];*/
+    print '<pre>Inserção dos pesos manualmente:<br>';
+    //H1
+    $pesos[0] = [-0.46, -0.07,  0.22];
+    //H2
+    $pesos[1] = [ 0.10,  0.94,  0.46];
+    //01
+    $pesos[2] = [ 0.78, -0.22,  0.58];
+    //02
+    $pesos[3] = [ 0.78, -0.22,  0.58];
 
-    /*Exemplo 3*/
-    // $entradas = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]];
-    // $resultadoEsperado = [0,1,1,0];
-    // $pesos_h = [[1.81,-5.61,-5.90],[5.27,-3.70,-3.72]];
-    // $pesos_o = [-2.48,-7.01,5.92];
+    $entradas = [[0,0],[0,1],[1,0],[1,1]];
 
-    $MSE = new RedeNeural($entradas, $resultadoEsperado, $resultadoEsperado_2, $pesos_h, $pesos_o);
+    $resultados_esperados = [[0,1],[1,0],[1,0],[0,1]];
+                
+    $MSE = new RedeNeural($pesos);
 
-    $resultMse = $MSE->atualizaPesos();
+    $MSE->treinar($entradas,$resultados_esperados,0.3,0.1);
 
-    // print 'Mean Squared Error (MSE): '.$resultMse.'% <br>';
+    print_r($MSE->MLP($entradas[0]));
+    print_r($MSE->MLP($entradas[1]));
+    print_r($MSE->MLP($entradas[2]));
+    print_r($MSE->MLP($entradas[3]));
 
-    // print 'Erro quadrático: '.$MSE->getErroQuadratico().'<br>';
-
-    // $resultados = $MSE->getResultados();
-    // print 'Resultados: ';
-    // print_r($resultados);
+    /*
+    Resultado Esperado = 0,0
+    [0] => 0.0098746961706474  O1 <- saída que importa
+    [1] => 0.99011562275379    O2 <- saída que importa
+    [2] => 0.97283841299981    H1
+    [3] => 0.01846863631561    H2
     
+    Resultado Esperado = 1,0
+    [0] => 0.98851079891453    O1 <- saída que importa
+    [1] => 0.011500471298918   O2 <- saída que importa
+    [2] => 0.026845065044502   H1
+    [3] => 8.0102581066593E-6  H2 
+    
+    Resultados Esperados = 1,0
+    [0] => 0.99058560277932    O1 <- saída que importa
+    [1] => 0.0094237202552812  O2 <- saída que importa
+    [2] => 0.99998296934959    H1
+    [3] => 0.97242032319106    H2
+
+    Resultados Esperados = 0,1
+    [0] => 0.0090253628665525  O1 <- saída que importa
+    [1] => 0.99096560471334    O2 <- saída que importa
+    [2] => 0.9783656951525     H1
+    [3] => 0.014788149169334   H2
+    */
+
+    print '<br>Inserção de pesos randomicamente:<br>';
+
+    for ($i=0; $i<4 ; $i++) { 
+        for ($j=0; $j<3 ; $j++) { 
+            $pesos[$i][$j] = rand(-5,5);
+        }
+    }
+
+    $entradas = [[0,0],[0,1],[1,0],[1,1]];
+
+    $resultados_esperados = [[0,1],[1,0],[1,0],[0,1]];
+                
+    $MSE = new RedeNeural($pesos);
+
+    $MSE->treinar($entradas,$resultados_esperados,0.3,0.1);
+
+    print_r($MSE->MLP($entradas[0]));
+    print_r($MSE->MLP($entradas[1]));
+    print_r($MSE->MLP($entradas[2]));
+    print_r($MSE->MLP($entradas[3]));
+
+    /*
+    Resultado Esperado = 0,0
+    [0] => 0.0092968141576208  O1 <- saída que importa
+    [1] => 0.99061320806223    O2 <- saída que importa
+    [2] => 0.999975976489      H1
+    [3] => 0.95315892128581    H2  
+    
+    Resultado Esperado = 1,0
+    [0] => 0.99058846453896    O1 <- saída que importa
+    [1] => 0.0094999883967769  O2 <- saída que importa
+    [2] => 0.97165709153301    H1
+    [3] => 0.018000147847865   H2   
+    
+    Resultados Esperados = 1,0
+    [0] => 0.99058524358826    O1 <- saída que importa
+    [1] => 0.0095032336608643  O2 <- saída que importa
+    [2] => 0.97104363906098    H1
+    [3] => 0.017446193750669   H2
+
+    Resultados Esperados = 0,1
+    [0] => 0.011516618656009    O1 <- saída que importa
+    [1] => 0.98837924168654     O2 <- saída que importa
+    [2] => 0.026876833497225    H1 
+    [3] => 1.5994240549239E-5   H2   
+    */
 ?>
